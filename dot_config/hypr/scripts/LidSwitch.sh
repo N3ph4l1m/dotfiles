4c9cd2954a86c9b96c482e37df575c85d7b04024
Richard Willox <rickywillox@gmail.com>
2023-10-01 21:07:57 +0100

    Upgrade

--- dot_config/hypr/scripts/LidSwitch.sh
+++ dot_config/hypr/scripts/LidSwitch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if grep open /proc/acpi/button/lid/LID/state; then
-    hyprctl keyword monitor "eDP-1, preferred, auto, 1"
+if grep open /proc/acpi/button/lid/LID0/state; then
+    hyprctl keyword monitor "eDP-1,2256x1504@59,3904x720,1.5"
 else
     if [[ `hyprctl monitors | grep "Monitor" | wc -l` != 1 ]]; then
         hyprctl keyword monitor "eDP-1, disable"
