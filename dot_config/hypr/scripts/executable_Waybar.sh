30bf45ec3c1e4a65d751663e4acc3dd54286751f
Richard Willox <rickywillox@gmail.com>
2023-10-09 21:55:56 +0100

    removing uneeded files

--- dot_config/hypr/scripts/executable_Waybar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CONFIG="$HOME/.config/hypr/waybar/config"
-STYLE="$HOME/.config/hypr/waybar/style.css"
-
-if [[ ! $(pidof waybar) ]]; then
-	waybar --bar main-bar --log-level debug --config ${CONFIG} --style ${STYLE}
-fi
